da8516df0b27a966b9eddca56bc60ed1b305fce0
llms26 <mxylms1210@163.com>
2024-10-23 16:55:51 +0800

    UPDATE complete the ll_cycle.c

--- lab/lab01/ll_cycle.c
+++ lab/lab01/ll_cycle.c
@@ -2,6 +2,26 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if(head == NULL){
+        return 0;
+    }
+
+    node* fast = head->next;
+    node* slow = head;
+
+    while(slow && fast){
+        if(fast == slow){
+            return 1;
+        }
+
+        if(fast->next == NULL){
+            return 0;
+        }
+        
+        fast = fast->next->next;
+        slow = slow->next;
+    
+    }
+
     return 0;
 }
